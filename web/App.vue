4e3a8fea29b1230249b2049df244d75162f441e0
Logic <38597904+LogicFan@users.noreply.github.com>
2024-10-06 11:30:42 -0400

    add side bar

--- web/App.vue
+++ web/App.vue
@@ -1,42 +1,92 @@
 <script setup lang="ts">
 import { ref } from "vue"
-import { useColors } from "vuestic-ui";
+import {
+    useColors,
+    VaSidebar,
+    VaSidebarItem,
+    VaSidebarItemContent,
+    VaSidebarItemTitle,
+    VaIcon,
+    VaSpacer
+} from "vuestic-ui";
 import Login from './components/Login.vue';
 import axios from "axios";
 
+// theme
 const { applyPreset, colors } = useColors();
 applyPreset('dark')
 
+// token
 const auth = ref(false);
-setInterval(() => {
-    let token = localStorage.getItem('token')
-    if (token != null) {
-        axios.post("/api/auth/refresh", {
-            token: token
-        }).then(response => {
-            localStorage.setItem('token', response.data)
-            auth.value = true
-        }).catch(_ => {
-            auth.value = false
-        })
-    } else {
-        auth.value = false
-    }
-}, 1000 * 60 * 10);
-
-function updateToken(token: string) {
+function setToken(token: string) {
     localStorage.setItem('token', token)
     auth.value = true
 }
+function rotateToken() {
+    let token = localStorage.getItem('token');
+    if (token == null) {
+        auth.value = false;
+    }
+
+    axios.post("/api/auth/refresh", {
+        token: token
+    }).then(response => {
+        setToken(response.data);
+    }).catch(_ => {
+        auth.value = false;
+    });
+}
+rotateToken();
+setInterval(() => {
+    rotateToken();
+}, 1000 * 60 * 10); // 10 minutes
+
+// sidebar
+const minSidebar = ref(false)
 </script>
 
 <template>
     <div class="background" :style="{ backgroundColor: colors.backgroundPrimary }">
         <template v-if="auth">
-            <div class="text-white text-lg"> Success </div>
+            <div class="flex">
+                <VaSidebar :minimized="minSidebar" minimized-width="64px" class="h-full min-h-screen">
+                    <div>
+                        <VaSidebarItem>
+                            <VaSidebarItemContent>
+                                <VaIcon name="ms-left_panel_close" />
+                                <VaSidebarItemTitle></VaSidebarItemTitle>
+                            </VaSidebarItemContent>
+                        </VaSidebarItem>
+                        <VaSidebarItem>
+                            <VaSidebarItemContent>
+                                <VaIcon name="ms-dashboard" />
+                                <VaSidebarItemTitle>Overview</VaSidebarItemTitle>
+                            </VaSidebarItemContent>
+                        </VaSidebarItem>
+                        <VaSidebarItem>
+                            <VaSidebarItemContent>
+                                <VaIcon name="ms-monitoring" />
+                                <VaSidebarItemTitle>Investments</VaSidebarItemTitle>
+                            </VaSidebarItemContent>
+                        </VaSidebarItem>
+                    </div>
+
+                    <VaSpacer />
+
+                    <VaSidebarItem>
+                        <VaSidebarItemContent>
+                            <VaIcon name="ms-settings" />
+                            <VaSidebarItemTitle>Settings</VaSidebarItemTitle>
+                        </VaSidebarItemContent>
+                    </VaSidebarItem>
+                </VaSidebar>
+
+                <div class="text-white text-lg"> Success </div>
+            </div>
+
         </template>
         <template v-else>
-            <Login @token="(value) => updateToken(value)" />
+            <Login @token="(value) => setToken(value)" />
         </template>
     </div>
 </template>
--- web/main.ts
+++ web/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
+import { createVuestic, createIconsConfig } from "vuestic-ui";
 
-createApp(App).mount("#app");
+createApp(App)
+    .use(createVuestic({
+        config: {
+            icons: createIconsConfig({
+                aliases: [],
+                fonts: [
+                    {
+                        name: 'ms-{icon}',
+                        resolve: ({ icon }: any) => ({
+                            class: 'material-symbols-rounded',
+                            content: icon,
+                            tag: 'span',
+                        })
+                    }
+                ]
+            }),
+        },
+    }))
+    .mount("#app");
