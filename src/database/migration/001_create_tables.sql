b9d0885ae7a37fd46ac2f1595f62b6c6be12958f
Logic <38597904+LogicFan@users.noreply.github.com>
2024-10-24 04:19:00 -0400

    fix sql

--- src/database/migration/001_create_tables.sql
+++ src/database/migration/001_create_tables.sql
@@ -1,15 +1,15 @@
-CREATE TABLE `Accounts` (
+CREATE TABLE `accounts` (
     `id` TEXT PRIMARY KEY NOT NULL,
     `acct` TEXT NOT NULL,
     `name` TEXT NOT NULL,
     `type` TEXT NOT NULL
 );
 
-CREATE TABLE `Transactions` (
+CREATE TABLE `transactions` (
     `id` TEXT PRIMARY KEY NOT NULL,
-    `account` TEXT NOT NULL REFERENCES `Accounts` (id),
+    `account` TEXT NOT NULL REFERENCES `accounts` (id),
     `date` DATE NOT NULL,
     `action` TEXT NOT NULL
 );
 
-CREATE INDEX `Transactions_0` ON `Transactions` (`date`);
+CREATE INDEX `transactions_i0` ON `transactions` (`date`);
