98d0ed6ed1a190d33a476ba8c1acff34a69060bd
Logic <38597904+LogicFan@users.noreply.github.com>
2024-10-24 02:11:58 -0400

    redesign main page

--- web/App.vue
+++ web/App.vue
@@ -8,84 +8,36 @@ import {
     VaSidebarItemTitle,
     VaIcon
 } from "vuestic-ui";
-import Login from './components/Login.vue';
-import axios from "axios";
 
 // dark theme
-const { applyPreset, colors } = useColors();
-applyPreset('dark');
-
-// auth token
-const auth = ref(false);
-function setToken(token: string) {
-    localStorage.setItem('token', token);
-    auth.value = true;
-}
-function refreshToken() {
-    let token = localStorage.getItem('token');
-    if (token == null) {
-        auth.value = false;
-    } else {
-        axios.post("/api/auth/refresh", {
-            token: token
-        }).then(response => {
-            setToken(response.data);
-        }).catch(_ => {
-            auth.value = false;
-        });
-    }
-}
-refreshToken();
-setInterval(() => {
-    refreshToken();
-}, 1000 * 60 * 10); // 10 minutes
-
-// sidebar
-const minimize = ref(false);
-const icon = computed(() => minimize.value ? 'ms-left_panel_open' : 'ms-left_panel_close')
+useColors().applyPreset('dark');
 </script>
 
 <template>
-    <div class="background" :style="{ backgroundColor: colors.backgroundPrimary }">
-        <template v-if="auth">
-            <div class="flex">
-                <div class="flex-grow-0">
-                    <VaSidebar :minimized="minimize" minimized-width="64px" class="h-full min-h-screen">
-                        <VaSidebarItem @click="minimize = !minimize">
-                            <VaSidebarItemContent>
-                                <VaIcon :name="icon" />
-                            </VaSidebarItemContent>
-                        </VaSidebarItem>
-                        <VaSidebarItem to="/" :active="$route.path == '/'">
-                            <VaSidebarItemContent>
-                                <VaIcon name="ms-dashboard" />
-                                <VaSidebarItemTitle>Overview</VaSidebarItemTitle>
-                            </VaSidebarItemContent>
-                        </VaSidebarItem>
-                        <VaSidebarItem to="/investments" :active="$route.path == '/investments'">
-                            <VaSidebarItemContent>
-                                <VaIcon name="ms-monitoring" />
-                                <VaSidebarItemTitle>Investments</VaSidebarItemTitle>
-                            </VaSidebarItemContent>
-                        </VaSidebarItem>
-                        <VaSidebarItem to="/settings" :active="$route.path == '/settings'">
-                            <VaSidebarItemContent>
-                                <VaIcon name="ms-settings" />
-                                <VaSidebarItemTitle>Settings</VaSidebarItemTitle>
-                            </VaSidebarItemContent>
-                        </VaSidebarItem>
-                    </VaSidebar>
-                </div>
-                <div class="p-4">
-                    <RouterView />
-                </div>
-            </div>
-        </template>
-        <template v-else>
-            <div>
-                <Login @token="(value) => setToken(value)" />
-            </div>
-        </template>
+    <div class="background" :style="{ backgroundColor: useColors().colors.backgroundPrimary }">
+        <VaNavbar>
+            <template #left>
+                <VaNavbarItem>
+                    <img src="@assets/icon.svg" alt="logo" height="32" width="32" />
+                </VaNavbarItem>
+            </template>
+
+            <template #center>
+                <VaNavbarItem>
+                    <VaButton icon="ms-dashboard" background-opacity="0" color="textPrimary" to="/">
+                        Overview
+                    </VaButton>
+                </VaNavbarItem>
+                <VaNavbarItem>
+                    <VaButton icon="ms-monitoring" background-opacity="0" color="textPrimary" to="/investments">
+                        Investments
+                    </VaButton>
+                </VaNavbarItem>
+            </template>
+        </VaNavbar>
+        <div class="p-4">
+            <RouterView />
+        </div>
     </div>
 </template>
 
@@ -98,8 +50,14 @@ const icon = computed(() => minimize.value ? 'ms-left_panel_open' : 'ms-left_pan
 }
 </style>
 <style lang="scss">
+// ensure browser scroll bar is dark
 :root {
     color-scheme: dark;
+}
+
+// make navigation bar slimmer
+.va-navbar {
+    --va-navbar-padding-y: 0.6rem;
 }
 
 .login-card {
