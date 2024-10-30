e238dc1beedf4f79ea02e7c87525ccb7c0bc4448
Logic <38597904+LogicFan@users.noreply.github.com>
2024-10-30 07:29:40 -0400

    responsive designs

--- web/components/Settings.vue
+++ web/components/Settings.vue
@@ -6,7 +6,7 @@ const authorize = defineModel<boolean>({ required: true })
 </script>
 
 <template>
-    <div class="px-4 gap-4 columns-4">
+    <div class="px-4 gap-4 columns-1 sm:columns-2 lg:columns-3 2xl:columns-4">
         <div class="setting-item">
             <Users v-model:model-value="authorize" />
         </div>
